9f38cb09b41bd5ef047f72d09d411cec292d63e5
Fábio Garrote <fgarrote12@gmail.com>
2025-05-29 19:03:33 -0300

    fix: fixing the db name

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from database import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atividades.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
